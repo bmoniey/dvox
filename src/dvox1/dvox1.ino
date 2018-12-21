c942c4c2c0788320487b32a7759f9e50409b97c5
b.moniey <b.moniey@gmail.com>
2018-12-20 21:15:03 -0800

    -Animated the display -added select and change action for the vox and delay controls -slowed down the display update to speed up the loop a bit

--- src/dvox1/dvox1.ino
+++ src/dvox1/dvox1.ino
@@ -22,7 +22,7 @@ Encoder enc((uint8_t)ENC_DIR_CW_POS,(int16_t)100,(int16_t)0,(int16_t)50);
 void encoderClkISR(void);
 void encoderSwISR(void);
 void updateDisplay(void);
-void updateThreshold(void);
+void doEncoder(void);
 
 //  Naming the pins I'm using for clarity.
 const int audioInPin = A1;  // Audio sense
@@ -31,11 +31,13 @@ const int sensitivity = A5; // threshold set POT
 const int PTToutPin = 13; // output to keying transistor
 
 //  declaring variables that we'll use
-uint32_t delayvalue = 0; // amount of time in hundreths of a second before dropping PTT
-uint32_t threshold = 0; // audio trigger level initial value
+int16_t delayvalue = 0; // amount of time in hundreths of a second before dropping PTT
+int16_t threshold = 0; // audio trigger level initial value
 int ptt = 0;  // variable for holding current PTT delay countdown
 uint16_t audio = 0;  // variable that will hold audio sense
 int PTT_ON; // Flag to indicate current PTT status
+int sel_idx,sel;//used to keep track of the menu
+int dspCntr;
 
 void setup() {
   Serial.begin(9600);
@@ -50,7 +52,7 @@ void setup() {
   // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
   display.display();
-  delay(2000);
+  delay(1000);
   
   pinMode(ENC_DT_PIN, INPUT_PULLUP);
   pinMode(ENC_CLK_PIN, INPUT_PULLUP);
@@ -63,26 +65,20 @@ void setup() {
   enc.setup(); 
   // Turn off PTT right away so we're not keying on startup
   PTT_ON = 0;
-  delay(1000);
+  delayvalue = 50;
+  threshold  = 400;
   display.clearDisplay();
 
-  updateThreshold();    
+  //sel,sel_item
+  sel=0;//0 not selected, selected
+  sel_idx = 0;//0=vox 1=delay
+  dspCntr = 0;//initialy the display update counter    
 }
 
 void loop() 
 {
     // Here we go. First read the pots and set variables
-    enc.isrReadSwPin();//check the switch on the encoder
-
-    if(enc.encDatAvail()){
-        updateThreshold();     
-        sprintf(cstr,"Encoder::Pos:%3d Dir:%2d threshold:%3d\n",enc.getPos(),enc.getDir(),threshold);
-        Serial.print(cstr);
-    }
-
- //delayvalue = analogRead(tailsetInPin) / 5; // yields 0 to 204
- delayvalue   = 500 / 5; // yields 0 to 204
- delayvalue   = delayvalue + 2; // adjusted to no less than 2, 1 after first pass through loop
+    doEncoder();
 
     // Check for audio. Set delay if present
  audio = analogRead(audioInPin);
@@ -110,9 +106,16 @@ void loop()
            PTT_ON = 0; // Remeber that we've turned it off
    } 
  }
- updateDisplay();
- delay(10);  // wait 10ms before looping again.  loop 100 times/second
-  
+
+ //update the display every 10 loops
+ dspCntr++;
+ if(dspCntr>9){
+  dspCntr=0;
+  updateDisplay();
+ }
+   
+ 
+ delay(10);  // wait 10ms before looping again.  loop 100 times/second 
 }
 
 void encoderClkISR(void){
@@ -151,11 +154,10 @@ void updateDisplay(){
   display.drawCircle(5, 40, 5, WHITE);
   //DLY:
   display.setCursor(15,32);
-  sprintf(cstr,"DLY:%4d",delayvalue*10);
+  sprintf(cstr,"DLY:%4d",delayvalue);
   display.println(cstr);
   
   //square
-  
   //PTT
   if(PTT_ON){
     display.fillRect(30,48,128-30-30,64-48, WHITE);
@@ -167,17 +169,59 @@ void updateDisplay(){
     display.setCursor(30+15,49);
     display.println(F("PTT"));
   }
+  //select dot
+  if(sel_idx){
+    //delay
+    if(sel){
+      display.fillCircle(5, 40, 3, WHITE);
+    }else
+    {
+      display.drawCircle(5, 40, 3, WHITE);
+    }
+  }else
+  {
+    //vox
+    if(sel){
+      display.fillCircle(5, 25, 3, WHITE);
+    }else{
+      display.drawCircle(5, 25, 3, WHITE);
+    }
+  }
   display.display();
 }
 
-void updateThreshold()
-{
-    //initialize the threshold
-  uint32_t pos;
-  pos =  enc.getPos();
-  pos = pos * 819;
-  pos = pos / 100;
-  threshold      = pos; // yields 0 to 819
-  threshold      = threshold + 70; // adjusted 70 to 889 
+
+void doEncoder(){
+     enc.isrReadSwPin();//check the switch on the encoder
+    if(enc.getBtnDn()){
+       enc.clrBtnDn();
+       sel = sel ? 0 : 1;
+      //sprintf(cstr,"Encoder::Button Down:%d,%d",sel,sel_idx);
+      //Serial.println(cstr);
+    }
+    
+    if(enc.encDatAvail()){
+        if(sel){
+          if(sel_idx == 0){
+            //adjust vox
+            threshold += enc.getDir()*10;
+            if(threshold > 820){ threshold = 820;}
+            if(threshold < 10 ){ threshold = 1;} 
+          }else if(sel_idx == 1){
+            //adjust delay
+            delayvalue += enc.getDir();
+            if(delayvalue > 100){delayvalue = 100;}
+            if(delayvalue < 1){delayvalue   = 1;}
+          }
+        }else{
+          sel_idx++;
+          if(sel_idx > 1){
+            sel_idx = 0;
+          }
+        }
+             
+        //sprintf(cstr,"Encoder::Pos:%3d Dir:%2d threshold:%3d delay:%3d\n",enc.getPos(),enc.getDir(),threshold,delayvalue);
+        //Serial.print(cstr);
+    }
 }
 
